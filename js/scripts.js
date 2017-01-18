0705b1092ff092fac21d9d169b8487e0de9e60b3
Renee Mei and Shruti Priya <renee@>
2017-01-17 16:00:01 -0800

    try to add javascript for recursive function

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
+var recursive = function(number)
+{
+  if (number === 0){
+    return 1;
+  }else{
+    if(number >1){
+    var result = number*recursive(number-1);
+  }else{
+    return result;
+  }
+}
+}
+
 $(document).ready(function(){
  $("form.factorial").submit(function(event){
    event.preventDefault();
-   var factorial=parseInt($("input").val());
-   var multiply = 1;
-   if (factorial){
-   for (var currentNum =1; currentNum <= factorial; currentNum += 1){
-     multiply = multiply*currentNum;
-   }
- }else{
-   alert("please enter a positive integer!");
- }
+   var factorial= parseInt($("input").val());
+  //  var multiply = 1;
+   var multiply = recursive(factorial);
+ //   if (factorial){
+ //   for (var currentNum =1; currentNum <= factorial; currentNum += 1){
+ //     multiply = multiply*currentNum;
+ //   }
+ // }else{
+ //   alert("please enter a positive integer!");
+ // }
    $("#result").text(multiply.toString());
  });
 });
